50efa8665719f95bbec261628f2ec3a6d2a37c6a
Suzzit <bajagainsuzzit@gmail.com>
2022-11-02 17:31:41 +0545

    Updates

---
+++ precache-manifest.0d8766723f3c3438d7b52afa721f1196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6cc1a3b901274c5381f",
+    "url": "/suzzitfolio/static/css/main.2690c870.chunk.css"
+  },
+  {
+    "revision": "d6cc1a3b901274c5381f",
+    "url": "/suzzitfolio/static/js/main.d6cc1a3b.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/suzzitfolio/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "9526bd040a01b5d3d667",
+    "url": "/suzzitfolio/static/js/2.9526bd04.chunk.js"
+  },
+  {
+    "revision": "d2f5e25be88f3e55ba04",
+    "url": "/suzzitfolio/static/js/runtime~main.d2f5e25b.js"
+  },
+  {
+    "revision": "a098d4bb70880496e614301174422d59",
+    "url": "/suzzitfolio/static/media/profile_image_final.a098d4bb.png"
+  },
+  {
+    "revision": "9208249f6f15659a1b580c8f073f3af6",
+    "url": "/suzzitfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abfb12813d6bc653608e5ce0daa26985.js"
+  "/suzzitfolio/precache-manifest.0d8766723f3c3438d7b52afa721f1196.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/suzzitfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
