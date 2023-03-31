ade95c7acdd1c162da46d82997f5478beeae0ff4
suzzit <bajagainsuzzit@gmail.com>
2023-04-01 00:19:11 +0545

    Updates

---
+++ precache-manifest.515dbf63cf7ee06e3dc5b3b85d1f8877.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c000b9fb1bfea1949c6",
+    "url": "/suzzitfolio/static/css/main.1005717e.chunk.css"
+  },
+  {
+    "revision": "9c000b9fb1bfea1949c6",
+    "url": "/suzzitfolio/static/js/main.9c000b9f.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/suzzitfolio/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "2da7a40b899f80078d36",
+    "url": "/suzzitfolio/static/js/2.2da7a40b.chunk.js"
+  },
+  {
+    "revision": "4f75742a72bb48122fd7",
+    "url": "/suzzitfolio/static/js/runtime~main.4f75742a.js"
+  },
+  {
+    "revision": "a098d4bb70880496e614301174422d59",
+    "url": "/suzzitfolio/static/media/profile_image_final.a098d4bb.png"
+  },
+  {
+    "revision": "c3690d7f5e19e66f3548d8bf743c802b",
+    "url": "/suzzitfolio/static/media/logo.c3690d7f.svg"
+  },
+  {
+    "revision": "dddab78d06f8fd39036f403d5ebf18bd",
+    "url": "/suzzitfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/suzzitfolio/precache-manifest.6b3f849fd3cce2cbe6e22e59194f8617.js"
+  "/suzzitfolio/precache-manifest.515dbf63cf7ee06e3dc5b3b85d1f8877.js"
 );
 
 workbox.clientsClaim();
